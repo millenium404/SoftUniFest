7a95dc6b4fe939afa34af582e92978828f5941cc
millenium404 <alarak.bg@protonmail.com>
2022-06-14 11:49:54 +0300

    env vars added

--- SoftUniFest/settings.py
+++ SoftUniFest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .env import env_key, env_debug, env_hosts
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-efk1#6#sdkz8$+^ncbft5!&^j@gf!0nd)(2u#nqs7_)_pzgpp0'
+SECRET_KEY = env_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env_debug
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env_hosts
 
 
 # Application definition
