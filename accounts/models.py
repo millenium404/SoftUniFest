7ad3d4837a60433fa4a00084d820ec78a3a7f169
millenium404 <alarak.bg@protonmail.com>
2022-06-17 12:01:43 +0300

    accounts and main apps added

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    reg_date = models.DateTimeField(auto_now_add=True)
+    card_number = models.CharField(max_length=20, blank=True, null=True)
+    card_expires = models.CharField(max_length=5, blank=True, null=True)
+    notifications = models.BooleanField(default=False)
+    is_staff = models.BooleanField(blank=True, null=True)
+    is_client = models.BooleanField(blank=True, null=True)
+    is_merchant = models.BooleanField(blank=True, null=True)
