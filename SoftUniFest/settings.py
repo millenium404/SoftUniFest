290125e1bd68a8d5945d8d3733afc0a39d15fce8
millenium404 <alarak.bg@protonmail.com>
2022-06-18 17:32:27 +0300

    fixes

--- SoftUniFest/settings.py
+++ SoftUniFest/settings.py
@@ -120,8 +120,7 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_URL = '/static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
